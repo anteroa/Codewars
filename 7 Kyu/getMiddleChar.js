46d171461c346912b5cdda1603f065aa5a3e740b
Antero <anteroavalos2508@gmail.com>
2022-05-27 10:37:39 -0700

    Return the middle character/s of a word.

---
+++ 7 Kyu/getMiddleChar.js
@@ -0,0 +1,30 @@
+/**You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out. */
+
+
+//my solution
+function getMiddle(s)
+{
+  //Code goes here!
+  let index = Math.floor(s.length / 2)
+  if(s.length % 2 == 0){return s[index-1]+s[index]}
+  else if( s.length % 2 == 1){return s[index]}
+}
+
+//fancy shmancy solution
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
