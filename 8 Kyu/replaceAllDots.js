99c703b66fd5ed9810cd96e49d4c737b24015648
Antero A <anteroavalos2508@gmail.com>
2022-06-18 23:47:01 -0700

    Replace all dots in a string with dashes

---
+++ 8 Kyu/replaceAllDots.js
@@ -0,0 +1,25 @@
+/**The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null. */
+
+//solution
+var replaceDots = function(str) {
+    for(let i = 0; i < str.length; i++){
+      if(str[i] == '.'){
+        str = str.replace(str[i], '-')
+      }
+    }
+    return str
+  }
+  
+
+//regex solution 
+var replaceDots = function(str) {
+    return str.replace(/\./g,'-');
+  }
